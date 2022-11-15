01653e7c840f94bfcb8d2402eac634ffe656d0bd
Jing Mai <jingmai@pku.edu.cn>
2022-11-15 21:03:52 +0800

    set keymaps, and `clang-format` config for `null-ls`

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,17 +8,43 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+-- code action sources
+local code_actions = null_ls.builtins.code_actions
+
+-- hover sources
+local hover = null_ls.builtins.hover
+
+-- completion sources
+local completion = null_ls.builtins.completion
+
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
-  debug = false,
+  debug = true,
   sources = {
+    -- formatting
     formatting.prettier.with {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
+    formatting.clang_format.with {
+      extra_args = { "-style=file" }
+    }, -- c/c++ formatter
     formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
+    formatting.stylua, -- lua formatter
     formatting.google_java_format,
-    diagnostics.flake8,
+    formatting.shfmt, -- shell formatter
+
+    -- diagnostics
+    diagnostics.cspell, -- spell check
+    diagnostics.flake8, -- python linter
+    diagnostics.shellcheck, -- shell linter 
+
+    -- code actions 
+    code_actions.cspell, -- spell check
+    code_actions.gitsigns,
+    code_actions.shellcheck, -- shell script code actions
+
+    -- hover dictionary
+
   },
 }
